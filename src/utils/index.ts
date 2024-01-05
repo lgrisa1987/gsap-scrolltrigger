7d6f41474c5e4f7410640a0317b31ad6f3c93180
Leandro <leagrisalena@gmail.com>
2024-01-05 16:29:23 -0300

    ajustes varios

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,21 @@
+/* Separa el título en letras separadas en <span/> */
 export const getLetters = (heading: HTMLElement) => {
   try {
     if (heading)
       return heading.textContent
-        .split('')
-        .map(
-          (letter) =>
-            `<span class='inline-block translate-y-full rotate-45'>${letter}</span>`
-        )
-        .join('');
+        .split("")
+        .map((letter) => `<span class='inline-block translate-y-full rotate-45'>${letter}</span>`)
+        .join("");
   } catch (error) {
-    console.log('El elemento html no existe');
+    console.log("El elemento html no existe");
   }
 };
+
+export const textAnimation = (delay = 0) => ({
+  y: 0,
+  rotation: 0,
+  ease: "expo",
+  duration: 1,
+  delay: delay,
+  stagger: 0.1,
+});
